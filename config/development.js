1226484d3b4cfbcf6afb5c759a04906cfa668e9f
D-cat1 <72681912+D-cat1@users.noreply.github.com>
2022-04-09 23:14:05 +0700

    Update development.js

--- config/development.js
+++ config/development.js
@@ -5,7 +5,7 @@ module.exports = {
 	mongoUrl: 'mongodb://dcat:dcat@dcat-shard-00-00.672tp.mongodb.net:27017,dcat-shard-00-01.672tp.mongodb.net:27017,dcat-shard-00-02.672tp.mongodb.net:27017/testdb?ssl=true&replicaSet=atlas-124x0p-shard-0&authSource=admin&retryWrites=true&w=majority',
 	insightApi: 'https://insight.wificoin.club/insight-api',
 	wfcPayUrl: 'https://wfc.wificoin.club/#/wifiPortal/payment?authServer=',
-	authDomain: 'http://172.96.252.145:8080',
+	authDomain: 'https://wifidogfd.herokuapp.com',
 	wfcAuthPath: '/wifidog/auth/wfc',
 	wxAuthPath: '/wifidog/auth/weixin',
 	device_dir: './public/device',
