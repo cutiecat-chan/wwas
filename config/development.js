957fc61d91b822be94fb20585854a332839e29fe
D-cat1 <72681912+D-cat1@users.noreply.github.com>
2022-04-09 22:15:07 +0700

    Update development.js

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: process.env.PORT || 3000,
-	mongoUrl: 'mongodb+srv://dcat:dcat@dcat.672tp.mongodb.net/testauth?retryWrites=true&w=majority',
+	mongoUrl: 'mongodb://dcat:dcat@dcat-shard-00-00.672tp.mongodb.net:27017,dcat-shard-00-01.672tp.mongodb.net:27017,dcat-shard-00-02.672tp.mongodb.net:27017/testdb?ssl=true&replicaSet=atlas-124x0p-shard-0&authSource=admin&retryWrites=true&w=majority',
 	insightApi: 'https://insight.wificoin.club/insight-api',
 	wfcPayUrl: 'https://wfc.wificoin.club/#/wifiPortal/payment?authServer=',
 	authDomain: 'http://172.96.252.145:8080',
